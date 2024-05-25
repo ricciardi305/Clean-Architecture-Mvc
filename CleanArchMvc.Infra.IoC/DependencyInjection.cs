2178e6d5845a675c4eae5b96dd402898f072eddc
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2024-05-25 01:54:02 -0300

    feat: Add Db Connection, apply migration and insert data into tables

---
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            );
+
+            service.AddScoped<ICategoryRepository, CategoryRepository>();
+            service.AddScoped<IProductRepository, ProductRepository>();
+
+            return service;
+        }
+    }
+}
